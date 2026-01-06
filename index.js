3b695f9e87b567bff250495462244da022800d5c
sidneynyambok899-a11y <sidneynyambok899@gmail.com>
2026-01-06 07:44:13 +0300

    intial commit

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+require('datejs');
 
-
+function combineUsers(...args) {
+  const combinedObject = { users: [] };
+  for (const array of args) {
+    combinedObject.users.push(...array);
+  }
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
